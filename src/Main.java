74d45693c0504c1bed63dca5997a964a6ce96c1c
Jeff Moya <jeffmoya@amazon.com>
2022-10-24 13:43:08 -0700

    new repo

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        Game app = new Game();
+        app.execute();
+
+    }
+}
